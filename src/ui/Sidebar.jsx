11f0569f86190ad7e706bbf374a36b6c48c11583
tu <tu_kaihui@163.com>
2025-11-17 23:00:36 +0800

    去掉上传测试数据按钮

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,7 +1,7 @@
 import Logo from "./Logo";
 import styled from "styled-components";
 import MainNav from "./MainNav";
-import Uploader from "../data/Uploader";
+// import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
   background-color: var(--color-grey-0);
@@ -19,7 +19,7 @@ function Sidebar() {
       <Logo />
       {/* 常规设置导航的模式 */}
       <MainNav />
-      <Uploader />
+      {/* <Uploader /> */}
     </StyledSidebar>
   );
 }
