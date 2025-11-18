f04bbfe327a3ac17cefd23950665b83ecb89bdfb
tu <tu_kaihui@163.com>
2025-11-18 10:21:38 +0800

    error resolved

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -2,7 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateUser as updateUserApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
-export default function useUpdateUser() {
+export function useUpdateUser() {
   const queryClient = useQueryClient();
   const { mutate: updateUser, isLoading } = useMutation({
     mutationFn: updateUserApi,
